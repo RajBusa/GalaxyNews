6770b795c6d7ee579a0772b18f85f6fa0eb04631
RajBusa <20it0112charusat.edu.in>
2022-04-02 09:55:27 +0530

    first commit

---
+++ lib/helper/data.dart
@@ -0,0 +1,147 @@
+import '../models/category_model.dart';
+
+// function that returns List<CategoryModel>
+
+List<CategoryModel> getCategories() {
+  // ignore: deprecated_member_use
+  List<CategoryModel> category =
+      <CategoryModel>[]; // creating list for returning
+
+  //1
+  CategoryModel categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "General";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "Business";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "Finance";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "Marketing";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "Advertising";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "Trade";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "Politics";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "Government";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+  //2
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "Entertainment";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "Culture";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "Art";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+
+  //3
+
+  //4
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "Health";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+
+  //5
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "Science";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+
+  //5
+
+  //5
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "Technology";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "Computers";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "IT Business";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "Technical Reviews";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+  
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "Robotics & Automation";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+  //6
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "War";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+
+
+
+
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "Chemistry";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+
+  categoryModel = new CategoryModel();
+  categoryModel.CategoryName = "Sports";
+  categoryModel.imageUrl =
+      "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  category.add(categoryModel);
+  // categoryModel = new CategoryModel();
+  // categoryModel.CategoryName = "Cricket";
+  // categoryModel.imageUrl =
+  //     "https://images.unsplash.com/photo-1617957796155-72d8717ac882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80";
+  // category.add(categoryModel);
+  return category;
+}
